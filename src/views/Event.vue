a93ac9fef753ebcac3bebf56d14d5c0094ec3fe6
pandorastrum <primeintegerslab@gmail.com>
2021-07-27 23:55:23 +0600

    modified: Single Event page

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <h1>Selected Title</h1>
+    <h1>The Event Id is : {{ title }}</h1>
+    <span
+      >Call the api with axios to get the event with id {{ title }} on the
+      created lifecycle hook</span
+    >
   </div>
 </template>
 
 <script>
 export default {
   name: "Event",
+  computed: {
+    title() {
+      return this.$route.params.id;
+    },
+  },
 };
 </script>
 
